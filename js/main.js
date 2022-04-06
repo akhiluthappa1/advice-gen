650ea3de997eadd2f277a0ff5a96acf0592286cb
akhiluthappa1 <akhiluthappa96@gmail.com>
2022-04-06 19:16:39 +0530

    web complete:

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 const button = document.querySelector("input")
 button.onclick = function() {
-    alert("Hello, World!");
-    document.querySelector(".number").innerHTML = "LMAO";
+  // alert("Hello, World!");
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  var response = 'madorchod'
+  var new_result = ''
+  
+  respone = fetch("https://api.adviceslip.com/advice", requestOptions)
+    .then(response => response.text())
+    .then(result => {console.log(result)
+          new_result=JSON.parse(result)
+          document.querySelector(".number").innerHTML = "A D V I C E # "+new_result["slip"]["id"]
+          document.querySelector(".advice_text").innerHTML = new_result["slip"]["advice"]})
+    .catch(error => console.log('error', error));
   }
